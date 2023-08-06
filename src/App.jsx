2ff250a837c63e95b2e7bdf20d6259382613b1a7
Piero Coronado <pacoronado01@gmail.com>
2023-08-06 19:49:08 -0400

    Edit route definitions to use RootLayout component for wrapping navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
+import RootLayout from "./pages/RootLayout";
+import League from "./pages/League";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "/league/:leagueId", element: <League /> },
+		],
+	},
+]);
 
 function App() {
 	return <RouterProvider router={router} />;
