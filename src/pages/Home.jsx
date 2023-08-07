298851e83d8124e7e590bfe76143bbb3cd6f0f66
Piero Coronado <pacoronado01@gmail.com>
2023-08-06 21:17:01 -0400

    Add accordion element to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,58 @@
-import { Container, Row, Col } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
+import { Link } from "react-router-dom";
+import Stack from "react-bootstrap/Stack";
 
 export default function Home() {
 	return (
-		<Container>
+		<Container className="mt-2">
 			<Row>
-				<Col>Home Page</Col>
+				<Col s={12} md={3}>
+					<Accordion defaultActiveKey="0">
+						<Accordion.Item eventKey="0">
+							<Accordion.Header>Leagues</Accordion.Header>
+							<Accordion.Body>
+								<Stack gap={2} className="">
+									<Link
+										to="/league/epl"
+										className="link-underline link-underline-opacity-0"
+									>
+										Premier League
+									</Link>
+									<Link
+										to="/league/laliga"
+										className="link-underline link-underline-opacity-0"
+									>
+										La Liga
+									</Link>
+									<Link
+										to="/league/seriea"
+										className="link-underline link-underline-opacity-0"
+									>
+										Serie A
+									</Link>
+									<Link
+										to="/league/bundesliga"
+										className="link-underline link-underline-opacity-0"
+									>
+										Bundesliga
+									</Link>
+									<Link
+										to="/league/ligue1"
+										className="link-underline link-underline-opacity-0"
+									>
+										Ligue 1
+									</Link>
+								</Stack>
+							</Accordion.Body>
+						</Accordion.Item>
+					</Accordion>
+				</Col>
+				<Col s={12} md={{ span: 8, offset: 1 }}>
+					Home Page
+				</Col>
 			</Row>
 		</Container>
 	);
