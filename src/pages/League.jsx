0ecd73cf18fba0e205c18c75727c269d25e72ded
Piero Coronado <pacoronado01@gmail.com>
2023-08-07 12:45:19 -0400

    Refactor League page

--- src/pages/League.jsx
+++ src/pages/League.jsx
@@ -1,15 +1,17 @@
 import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import LeagueNavigation from "../components/LeagueNavigation";
+import LeagueInfo from "../components/LeagueInfo";
 
 export default function League() {
 	const params = useParams();
 
 	return (
-		<Container>
-			<Row>
-				<Col>{params.leagueId}</Col>
+		<Container className="my-5">
+			<Row className="justify-content-evenly">
+				<LeagueNavigation selected={params.leagueId} />
+				<LeagueInfo />
 			</Row>
 		</Container>
 	);
