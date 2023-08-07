2a72c25e60ce199f725fdb5459a75a731c412895
Piero Coronado <pacoronado01@gmail.com>
2023-08-06 20:51:46 -0400

    Add font awesome icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
