cbc6d7e5b628dfe38c50a2c47d610a41a0fb5380
Piero Coronado <pacoronado01@gmail.com>
2023-08-06 20:51:17 -0400

    Edit Navbar appearance

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,28 +1,43 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFutbol,
+	faChartSimple,
+	faNewspaper,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function MainNavigation() {
 	return (
-		<Navbar expand="lg" className="bg-dark" data-bs-theme="dark">
-			<Container>
-				<Navbar.Brand>KickOff</Navbar.Brand>
+		<Navbar expand="lg" className="bg-dark py-md-3" data-bs-theme="dark">
+			<Container fluid>
+				<Navbar.Brand>
+					<span className="text-success">
+						Kick
+						<FontAwesomeIcon icon={faFutbol} />
+						ff
+					</span>
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
 						<LinkContainer to="/">
-							<Nav.Link>Home</Nav.Link>
+							<Nav.Link className="fs-5">
+								<FontAwesomeIcon icon={faFutbol} /> Football
+							</Nav.Link>
 						</LinkContainer>
-						<NavDropdown title="Leagues" id="basic-nav-dropdown">
-							<LinkContainer to="/league/epl">
-								<NavDropdown.Item>Premier League</NavDropdown.Item>
-							</LinkContainer>
-							<LinkContainer to="/league/seriea">
-								<NavDropdown.Item>Serie A</NavDropdown.Item>
-							</LinkContainer>
-							<LinkContainer to="/league/laliga">
-								<NavDropdown.Item>La Liga</NavDropdown.Item>
-							</LinkContainer>
-						</NavDropdown>
+						<LinkContainer to="/stats">
+							<Nav.Link className="fs-5">
+								<FontAwesomeIcon icon={faChartSimple} /> Statistics
+							</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/news">
+							<Nav.Link className="fs-5">
+								<FontAwesomeIcon icon={faNewspaper} /> News
+							</Nav.Link>
+						</LinkContainer>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
