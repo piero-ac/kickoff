c6e8d76258a8b9df66503fbe700b0f69152cad86
Piero Coronado <pacoronado01@gmail.com>
2023-08-07 13:51:02 -0400

    Convert buttons to links

--- src/components/LeagueInfo.jsx
+++ src/components/LeagueInfo.jsx
@@ -2,9 +2,12 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
+import { Link, useLocation } from "react-router-dom";
 
-export default function LeagueInfo() {
+export default function LeagueInfo({ leagueId, children }) {
+	const location = useLocation();
+	const [, , , contentType] = location.pathname.split("/");
+
 	return (
 		<Col s={12} md={8} className="bg-dark px-2 py-3 rounded-3">
 			<Row>
@@ -14,18 +17,43 @@ export default function LeagueInfo() {
 						gap={3}
 						className="justify-content-center justify-content-md-start"
 					>
-						<Button className="rounded-4 px-3" variant="secondary">
+						<Link
+							to={`/league/${leagueId}/overview`}
+							className={
+								"btn rounded-4 px-3" +
+								(contentType === "overview"
+									? "btn btn-success active"
+									: "btn btn-secondary")
+							}
+						>
 							Overview
-						</Button>
-						<Button className="rounded-4 px-3" variant="secondary">
+						</Link>
+						<Link
+							to={`/league/${leagueId}/fixtures`}
+							className={
+								"btn rounded-4 px-3" +
+								(contentType === "fixtures"
+									? "btn btn-success active"
+									: "btn btn-secondary")
+							}
+						>
 							Fixtures
-						</Button>
-						<Button className="rounded-4 px-3" variant="secondary">
+						</Link>
+						<Link
+							to={`/league/${leagueId}/table`}
+							className={
+								"btn rounded-4 px-3" +
+								(contentType === "table"
+									? "btn btn-success active"
+									: "btn btn-secondary")
+							}
+						>
 							Table
-						</Button>
+						</Link>
 					</Stack>
 				</Col>
 			</Row>
+			<Row>{children}</Row>
 		</Col>
 	);
 }
