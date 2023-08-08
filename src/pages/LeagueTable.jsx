6be0a8896b1f4a961214230272428c678c28427f
Piero Coronado <pacoronado01@gmail.com>
2023-08-08 18:55:21 -0400

    Use Standings component for displaying table

--- src/pages/LeagueTable.jsx
+++ src/pages/LeagueTable.jsx
@@ -1,5 +1,4 @@
-import Container from "react-bootstrap/Container";
-import Table from "react-bootstrap/Table";
+import Standings from "../components/Standings";
 import { useLoaderData, useLocation, json } from "react-router-dom";
 
 export default function LeagueTable() {
@@ -8,88 +7,7 @@ export default function LeagueTable() {
 	const data = useLoaderData();
 	const table = data.data;
 
-	const getRankClassName = (rank, leagueId) => {
-		if (rank === 1) return "text-success";
-
-		if (leagueId === "78") {
-			if (rank === 16) {
-				return "text-warning";
-			} else if (rank >= 17) {
-				return "text-danger";
-			} else {
-				return "";
-			}
-		} else if (leagueId === "61") {
-			if (rank >= 16) {
-				return "text-danger";
-			} else {
-				return "";
-			}
-		} else {
-			return rank > 17 ? "text-danger" : "";
-		}
-	};
-
-	return (
-		<Container fluid className="mx-1 my-2">
-			<Table variant="dark">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th colSpan={2}>Team</th>
-						<th>P</th>
-						<th>W</th>
-						<th>D</th>
-						<th>L</th>
-						<th className="d-none d-md-table-cell">F</th>
-						<th className="d-none d-md-table-cell">A</th>
-						<th>GD</th>
-					</tr>
-				</thead>
-				<tbody>
-					{table.map((team) => {
-						const rankClassName = getRankClassName(team.teamRank, leagueId);
-
-						return (
-							<tr key={team.teamId}>
-								<td className={rankClassName}>{team.teamRank}</td>
-								<td className="d-none d-md-table-cell">
-									<img
-										className="img-fluid"
-										width="25px"
-										src={team.teamLogo}
-										alt={`${team.teamName} Logo`}
-									/>
-								</td>
-								<td className="d-table-cell d-md-none"></td>
-								<td>{team.teamName}</td>
-								<td>{team.totalPoints}</td>
-								<td>{team.totalGamesWon}</td>
-								<td>{team.totalGamesDraw}</td>
-								<td>{team.totalGamesLose}</td>
-								<td className="d-none d-md-table-cell">{team.totalGoalsFor}</td>
-								<td className="d-none d-md-table-cell">
-									{team.totalGoalsAgainst}
-								</td>
-								<td>{team.totalGoalsDiff}</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</Table>
-			{leagueId === "78" && (
-				<>
-					<p className="text-warning">Rank 16 Goes to Relegation Playoffs</p>
-					<p className="text-danger">Ranks 17 and 18 are relegated</p>
-				</>
-			)}
-			{leagueId !== "78" && (
-				<>
-					<p className="text-danger">Bottom Three are relegated</p>
-				</>
-			)}
-		</Container>
-	);
+	return <Standings leagueId={leagueId} standings={table} />;
 }
 
 export async function loader({ params }) {
@@ -105,3 +23,6 @@ export async function loader({ params }) {
 		return response;
 	}
 }
+
+// Add deferring to show spinner when data is being fetched
+// Add errorLoader util to prevent invalid requests of tables for unsupported leagues
