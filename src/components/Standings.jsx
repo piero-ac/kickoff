70a1bb7c220b44df95ae64aec0b067cfd92ccd01
Piero Coronado <pacoronado01@gmail.com>
2023-08-09 11:36:20 -0400

    Add props to Standings for limiting table data

--- src/components/Standings.jsx
+++ src/components/Standings.jsx
@@ -3,7 +3,21 @@ import StandingsRow from "./StandingsRow";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 
-export default function Standings({ leagueId, standings }) {
+export default function Standings({ leagueId, standings, noText, limit }) {
+	if (!standings || standings.length === 0) {
+		return <p className="text-light text-center">Table not available yet</p>;
+	}
+
+	const subsetOfStandings = (limit, standings) => {
+		let subset = [];
+		for (let i = 0; i < limit; i++) {
+			subset.push(standings[i]);
+		}
+		return subset;
+	};
+
+	const standingsData = limit ? subsetOfStandings(limit, standings) : standings;
+
 	return (
 		<Container fluid className="mx-1 my-2">
 			<Table variant="dark">
@@ -21,18 +35,18 @@ export default function Standings({ leagueId, standings }) {
 					</tr>
 				</thead>
 				<tbody>
-					{standings.map((team) => (
+					{standingsData.map((team) => (
 						<StandingsRow key={team.teamId} team={team} leagueId={leagueId} />
 					))}
 				</tbody>
 			</Table>
-			{leagueId === "78" && (
+			{leagueId === "78" && !noText && (
 				<>
 					<p className="text-warning">Rank 16 Goes to Relegation Playoffs</p>
 					<p className="text-danger">Ranks 17 and 18 are relegated</p>
 				</>
 			)}
-			{leagueId !== "78" && (
+			{leagueId !== "78" && !noText && (
 				<>
 					<p className="text-danger">Bottom Three are relegated</p>
 				</>
