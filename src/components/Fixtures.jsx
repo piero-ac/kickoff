3a7f7e0770aac797c28dd007985acc828caf9ae8
Piero Coronado <pacoronado01@gmail.com>
2023-08-09 11:35:25 -0400

    Add message prop for Fixtures component

--- src/components/Fixtures.jsx
+++ src/components/Fixtures.jsx
@@ -2,14 +2,18 @@
 import FixtureRow from "./FixtureRow";
 import Container from "react-bootstrap/Container";
 
-export default function Fixtures({ fixtures }) {
+export default function Fixtures({ fixtures, message }) {
 	return (
 		<Container fluid>
 			{fixtures.matches.length > 0 &&
 				fixtures.matches.map((match, index) => {
 					return <FixtureRow key={index} match={match} />;
 				})}
-			{fixtures.matches.length === 0 && <p>No fixtures available yet.</p>}
+			{fixtures.matches.length === 0 && (
+				<p className="text-light text-center">
+					{message || "No fixtures available yet."}
+				</p>
+			)}
 		</Container>
 	);
 }
