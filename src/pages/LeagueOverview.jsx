88ffe7b62a116e19f936004b4d82ff08e3f1e7b8
Piero Coronado <pacoronado01@gmail.com>
2023-08-10 18:24:14 -0400

    Add localstorage storing with expiration and match filtering

--- src/pages/LeagueOverview.jsx
+++ src/pages/LeagueOverview.jsx
@@ -4,6 +4,7 @@ import { getLeagueName } from "../util/leagueName";
 import { getDate } from "../util/getDate";
 import { errorLoader } from "../util/errorLoader";
 import { Suspense } from "react";
+import { filterMatchesByDate } from "../util/filterMatches";
 import LoadingSpinner from "../UI/Spinner";
 import Fixtures from "../components/Fixtures";
 import Standings from "../components/Standings";
@@ -43,19 +44,63 @@ export default function LeagueOverview() {
 
 // Copied from LeagueTable
 async function loadStandings(leagueId) {
+	// Check if data is available in localStorage
+	const storedStandingsData = localStorage.getItem(`standings_${leagueId}`);
+	const storedStandingsDataExpiration = localStorage.getItem(
+		`standings_${leagueId}_expiration`
+	);
+	const currentTime = new Date().getTime();
+
+	if (
+		storedStandingsData &&
+		storedStandingsDataExpiration &&
+		currentTime >= storedStandingsDataExpiration
+	) {
+		console.log("Using localStorage for standings");
+		return JSON.parse(storedStandingsData);
+	}
+
+	// Otherwise, fetch from the backend
 	const endpoint = `/soccer/table/${leagueId}/2023`;
 	const response = await fetch("http://localhost:3000" + endpoint);
 
 	if (!response.ok) {
 		throw json({ message: "Could not fetch table." }, { status: 500 });
 	} else {
+		console.log("Using backend for standings");
 		const resData = await response.json();
-		return resData.data;
+		const { data, expiration } = resData;
+
+		// Store fetched data and its expiration
+		localStorage.setItem(`standings_${leagueId}`, JSON.stringify(data));
+		localStorage.setItem(`standings_${leagueId}_expiration`, expiration);
+
+		return data;
 	}
 }
 
 async function loadDayFixtures(leagueId) {
-	const endpoint = `/soccer/matches/${leagueId}/2023/${getDate()}`;
+	// Check if data is available in localStorage
+	const storedFixturesData = localStorage.getItem(`fixtures_${leagueId}`);
+	const storedFixturesDataExpiration = localStorage.getItem(
+		`fixtures_${leagueId}_expiration`
+	);
+	const currentTime = new Date().getTime();
+
+	if (
+		storedFixturesData &&
+		storedFixturesDataExpiration &&
+		currentTime >= storedFixturesDataExpiration
+	) {
+		// console.log(storedFixturesData);
+		const { matches } = JSON.parse(storedFixturesData);
+		// console.log(matches);
+		const filteredMatches = filterMatchesByDate(matches, getDate());
+		return { matches: filteredMatches };
+	}
+
+	// Otherwise, fetch from the backend
+	const endpoint = `/soccer/matches/${leagueId}/2023`;
 	const response = await fetch("http://localhost:3000" + endpoint);
 
 	if (!response.ok) {
@@ -65,7 +110,13 @@ async function loadDayFixtures(leagueId) {
 		);
 	} else {
 		const resData = await response.json();
-		return resData.data;
+		const { data, expiration } = resData;
+
+		// Store fetched data and its expiration
+		localStorage.setItem(`fixtures_${leagueId}`, JSON.stringify(data));
+		localStorage.setItem(`fixtures_${leagueId}_expiration`, expiration);
+		const filteredMatches = filterMatchesByDate(data.matches, getDate());
+		return { matches: filteredMatches };
 	}
 }
 
